71f3f97aeac25eb66615f52b75e1742c367ad76e
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-05-24 13:12:45 +0700

    Image Loading

--- app/src/main/java/com/rujirakongsomran/kt_coil/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_coil/MainActivity.kt
@@ -2,6 +2,11 @@ package com.rujirakongsomran.kt_coil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
 import com.rujirakongsomran.kt_coil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         setContentView(binding.root)
+
+        binding.imageView.load("https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg") {
+            crossfade(true)
+            crossfade(400)
+            transformations(
+                BlurTransformation(applicationContext, 20f),
+                RoundedCornersTransformation(10f)
+            )
+        }
+
+        binding.imageView2.load("https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg") {
+            transformations(RoundedCornersTransformation(10f))
+        }
+        binding.imageView3.load("https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg") {
+            transformations(
+                BlurTransformation(applicationContext, 20f),
+                CircleCropTransformation()
+            )
+        }
+        binding.imageView4.load("https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg") {
+            transformations(CircleCropTransformation())
+        }
+        binding.imageView5.load("https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg") {
+            transformations(
+                BlurTransformation(applicationContext, 20f),
+                GrayscaleTransformation()
+            )
+        }
+        binding.imageView6.load("https://cdn.pixabay.com/photo/2016/01/20/13/05/cat-1151519_960_720.jpg") {
+            transformations(GrayscaleTransformation())
+        }
+
     }
 }
